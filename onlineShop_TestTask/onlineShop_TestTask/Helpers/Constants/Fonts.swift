1d98911324a18752239b8bd8d7ca69826784bc9c
Zlata Guseva <gusevazlata96@gmail.com>
2023-03-11 22:18:15 +0400

    Add Fonts

---
+++ onlineShop_TestTask/onlineShop_TestTask/Helpers/Constants/Fonts.swift
@@ -0,0 +1,19 @@
+//
+//  Fonts.swift
+//  onlineShop_TestTask
+//
+//  Created by Zlata Guseva on 11.03.2023.
+//
+
+import UIKit
+
+extension UIFont {
+    static let bold26 = UIFont(name: "MontserratRoman-Bold", size: 26)
+    static let regular10 = UIFont(name: "MontserratRoman-Regular", size: 10)
+    static let bold14 = UIFont(name: "MontserratRoman-Bold", size: 14)
+//    static let bodyRegular = UIFont(name: "SFProText-Regular", size: 17)
+//    static let body = UIFont(name: "SFProText-Regular", size: 14)
+//    static let caption = UIFont(name: "SFProText-Regular", size: 10)
+//    static let titleLarge = UIFont(name: "SFProDisplay-Bold", size: 24)
+//    static let titleSmall = UIFont(name: "SFProDisplay-Medium", size: 17)
+}
