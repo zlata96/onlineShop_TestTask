00648d198a668be106706b27fd1b71121feb23e0
Zlata Guseva <gusevazlata96@gmail.com>
2023-03-18 23:23:41 +0400

    Add new assets

--- onlineShop_TestTask/onlineShop_TestTask/Helpers/Constants/Fonts.swift
+++ onlineShop_TestTask/onlineShop_TestTask/Helpers/Constants/Fonts.swift
@@ -8,6 +8,7 @@ extension UIFont {
     static let bold10 = UIFont(name: "MontserratRoman-Bold", size: 10)
     static let bold14 = UIFont(name: "MontserratRoman-Bold", size: 14)
     static let bold26 = UIFont(name: "MontserratRoman-Bold", size: 26)
+    static let bold28 = UIFont(name: "MontserratRoman-Bold", size: 28)
     static let regular8 = UIFont(name: "MontserratRoman-Regular", size: 8)
     static let regular10 = UIFont(name: "MontserratRoman-Regular", size: 10)
     static let regular12 = UIFont(name: "MontserratRoman-Regular", size: 12)
