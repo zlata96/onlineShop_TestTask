238452da052d9f8dcb3ec67d1e08e1279ac46ca9
Zlata Guseva <gusevazlata96@gmail.com>
2023-03-18 23:25:36 +0400

    Add ProductCategoryCollectionViewModel

---
+++ onlineShop_TestTask/onlineShop_TestTask/Model/ProductCategoriesCollectionViewModel.swift
@@ -0,0 +1,9 @@
+// ProductCategoriesCollectionViewModel.swift
+// onlineSHop_TestTask. Created by Zlata Guseva.
+
+import UIKit
+
+struct ProductCategoriesCollectionViewModel {
+    var name: String
+    var iconImage: UIImage?
+}
