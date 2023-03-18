024eae2a7c1350bf741582c30fbfdb1aa76e59c7
Zlata Guseva <gusevazlata96@gmail.com>
2023-03-18 23:24:54 +0400

    Add FlashSaleModel

---
+++ onlineShop_TestTask/onlineShop_TestTask/Model/FlashSaleModel.swift
@@ -0,0 +1,28 @@
+// FlashSaleModel.swift
+// onlineSHop_TestTask. Created by Zlata Guseva.
+
+import Foundation
+
+// MARK: - Welcome
+
+struct FlashSaleModel: Codable {
+    let flashSale: [FlashSale]
+
+    enum CodingKeys: String, CodingKey {
+        case flashSale = "flash_sale"
+    }
+}
+
+// MARK: - FlashSale
+
+struct FlashSale: Codable {
+    let category, name: String
+    let price: Double
+    let discount: Int
+    let imageURL: String
+
+    enum CodingKeys: String, CodingKey {
+        case category, name, price, discount
+        case imageURL = "image_url"
+    }
+}
